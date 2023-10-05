c477fbb23adcbadbfd2ef7e940ec2c60588e4f83
radusofron <radu.sofron169@gmail.com>
2023-10-05 19:43:57 +0300

    nav link activates accordingly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Function to save favourite theme
-function setFavouriteTheme() {
+function saveFavouriteTheme() {
   if (localStorage.getItem("darkMode") === "true") {
     localStorage.setItem("darkMode", "false");
   } else {
@@ -7,15 +7,54 @@ function setFavouriteTheme() {
   }
 }
 
-// Extract theme button
-const themeButton = document.getElementById("theme-checkbox");
+// Function to change website theme
+function changeTheme() {
+  // Extract theme button
+  const themeButton = document.getElementById("theme-checkbox");
 
-// Detect theme change
-themeButton.addEventListener("click", function () {
-  // Change theme
-  body.classList.toggle("dark");
-  sunIcon.classList.toggle("dark");
-  moonIcon.classList.toggle("dark");
-  // Save preference
-  setFavouriteTheme();
-});
+  // Detect theme change
+  themeButton.addEventListener("click", function () {
+    // Change theme
+    body.classList.toggle("dark");
+    sunIcon.classList.toggle("dark");
+    moonIcon.classList.toggle("dark");
+    // Save preference
+    saveFavouriteTheme();
+  });
+}
+
+// Function to detect article on viewport
+function detectViewportArticle() {
+  // Extract nav links
+  const navLinks = document.querySelectorAll(".nav-link");
+  // Extract article starting points
+  const articleStartingPoints = document.querySelectorAll(
+    ".article-starting-point"
+  );
+
+  // Save current article on viewport
+  let currentArticleId = "home";
+
+  window.addEventListener("scroll", function () {
+    // Detect changes of current article on viewport
+    articleStartingPoints.forEach((articleStartingPoint) => {
+      if (window.scrollY > articleStartingPoint.offsetTop) {
+        currentArticleId = articleStartingPoint.id;
+      }
+    });
+
+    // Style correspondent link
+    navLinks.forEach((navLink) => {
+      if (navLink.href.includes(currentArticleId)) {
+        // Change style only when correspondent link is not active
+        if (!navLink.classList.contains("active")) {
+          document.querySelector(".nav-link.active").classList.remove("active");
+          navLink.classList.add("active");
+        }
+      }
+    });
+  });
+}
+
+changeTheme();
+detectViewportArticle();
